b66e6f2182694d4c32b974ee4a5cd6fa3ea2689e
fernandomuller <31512583+fernandomuller@users.noreply.github.com>
2017-11-28 22:21:23 -0200

    Enviando PDU: Configuração e Dados.

---
+++ protocliente3.cc
@@ -0,0 +1,43 @@
+#include "protomau.h"
+
+using namespace std; 
+
+//	Simula a leitura dos sensores
+void atualizasensores(vector<TPsensor> vs){	
+
+		int t =vs.size();
+		printf("Lendo %d sensores...Atualizando...\n", t);		
+		if(t>0){
+				int id = 0;
+				for(int i = 0; i < t; i++){
+					id = vs[i].get_idSensor();
+					printf("Atualizando sensor id= %d. ", id);
+					vs[i].set_valor(vs[i].get_valor()+id);
+				  	cout<<"Valor atualizado = "<<vs[i].get_valor()<<endl;
+			   }		
+		}		
+}
+
+int main(int argc, char *argv[]){  	
+
+	protomau proto;	   	
+  	 	
+  	//Define sensores e valores iniciais(id e tipo)
+  	  	
+  	int N = 2;//Número de sensores
+  	
+  	vector<TPsensor> sensores; 
+  	TPsensor sensor[N];  
+  	  	
+   	for(int x=0; x<N; x++){
+   		sensor[x].set_idSensor(x+1);
+   		sensor[x].set_tipo((x+1)*10);
+   		sensores.push_back(sensor[x]);
+   	}  	
+  	
+  	proto.init(1001, sensores, atualizasensores); 
+			
+	//exit(0);	
+}
+
+
